0c4ced70dfc645b8efb3e9d01647b5d603ebeba5
patsy <scowper1406@gmail.com>
2026-05-19 07:30:28 -0400

    reorder folders

---
+++ nixos/nix_updateme.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/
+cd /home/patsy/Documents/my_notes
+git pull
+cd -
+cd /home/patsy/opt/patsy_configs
+git pull
+cd -
+cd /home/patsy/opt/homepage
+git pull
+cd -
+sudo nixos-rebuild switch --upgrade-all
+sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations +5
+sudo nix-collect-garbage
