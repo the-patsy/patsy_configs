acbbd09f0c056cc4c60af52b2d88996e6214fd2e
Patsy <scowper1406@gmail.com>
2026-06-07 09:55:54 -0400

    add HDD automount

--- nixos/nixos-config/hosts/powertower/default.nix
+++ nixos/nixos-config/hosts/powertower/default.nix
@@ -7,4 +7,16 @@
 
 hardware.graphics.enable = true;
 hardware.graphics.enable32Bit = true;  # needed for Steam on 64-bit systems
+
+#HDD
+boot.initrd.luks.devices."HDD" = {
+  device = "/dev/disk/by-uuid/c13b1aa8-dcde-499b-900d-9f59856f4f63";
+  };
+
+#Mount
+fileSystems."/home/patsy/HDD" = {
+  device = "/dev/mapper/HDD";
+  fsType = "btrfs";
+  options = [ "defaults" "nofail" ];
+  };
 }
