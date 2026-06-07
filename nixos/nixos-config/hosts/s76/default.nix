35b15373fa21d147895d586e150d30daa2416123
patsy <scowper1406@gmail.com>
2026-06-07 16:06:11 -0400

    update linux kernel to hosts

--- nixos/nixos-config/hosts/s76/default.nix
+++ nixos/nixos-config/hosts/s76/default.nix
@@ -4,7 +4,7 @@
   boot.initrd.luks.devices."luks-091aad14-94e8-4ef8-b53a-1493f3dfe3b6".device = "/dev/disk/by-uuid/091aad14-94e8-4ef8-b53a-1493f3dfe3b6";
 
   networking.hostName = "s76"; # Define your hostname.
-  
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 hardware.graphics.enable = true;
 hardware.graphics.enable32Bit = true;  # needed for Steam on 64-bit systems
 
