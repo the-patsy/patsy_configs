14acde1719a3ce27d99df622d8afdfdaaf877a49
patsy <scowper1406@gmail.com>
2026-06-07 13:46:33 -0400

    update updates

--- nixos/nixos-config/hosts/s76/default.nix
+++ nixos/nixos-config/hosts/s76/default.nix
@@ -3,7 +3,7 @@
 {
   boot.initrd.luks.devices."luks-091aad14-94e8-4ef8-b53a-1493f3dfe3b6".device = "/dev/disk/by-uuid/091aad14-94e8-4ef8-b53a-1493f3dfe3b6";
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "s76"; # Define your hostname.
   
 hardware.graphics.enable = true;
 hardware.graphics.enable32Bit = true;  # needed for Steam on 64-bit systems
