8a992f3435a7ac90ef1243b60a622c4d018983a2
patsy <scowper1406@gmail.com>
2026-09-16 19:52:17 -0400

    update update script

---
+++ gupdateme_powertower.sh
@@ -0,0 +1,16 @@
+#!/bin/bash/
+cd /home/patsy/HDD/Documents/my_notes
+git pull
+cd -
+cd /home/patsy/HDD/opt/patsy_configs
+git pull
+cd -
+cd /home/patsy/HDD/opt/homepage
+git pull
+cd -
+cd /home/patsy/HDD/opt/patsy_configs/nixos/nixos-config/
+sudo nix flake update
+cd -
+sudo nixos-rebuild switch --flake /home/patsy/HDD/opt/patsy_configs/nixos/nixos-config#powertower-gaming
+sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations +5
+sudo nix-collect-garbage
